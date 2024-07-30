82a57ef73d8224e204bdf3e9d90cd395cc7ea821
Damien Mégy <damien.megy@univ-lorraine.fr>
2024-07-30 22:54:47 +0200

    style : fab and level badge

--- js/components.js
+++ js/components.js
@@ -1,14 +1,3 @@
-function htmlPoints() {
-  let s = `
-		<span style="font-weight:400;position:relative">
-			${user.points} pt${user.points != 1 ? "s" : ""}
-			<span class="notif">
-                Niv. ${level(user.points)}
-            </span>
-		</span>`;
-  return s;
-}
-
 function htmlChapters() {
   let s = "";
   for (let i = 0; i < chapters.length; i++) {
